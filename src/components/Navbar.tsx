3999d61ef0568519383a9b6fc603e4b4eeabdfaa
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2023-10-21 13:31:58 +0700

    adjustment responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 const Navbar = () => {
   const user = false
   return (
-    <div className='h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase md:h-24'>
+    <div className='h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40'>
       <div className='hidden md:flex gap-4 flex-1'>
         <Link href="/">Home</Link>
         <Link href="/menu">Menu</Link>
@@ -19,10 +19,10 @@ const Navbar = () => {
       <div className='md:hidden'>
         <Menu />
       </div>
-      <div className='hidden md:flex gap-4 items-center flex-1'>
-        <div className='flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
+      <div className='hidden md:flex gap-4 items-center justify-end flex-1'>
+        <div className='md absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
           <Image src="/phone.png" alt="" width={20} height={20} />
-          <span>+62123</span>
+          <span>031-123456</span>
         </div>
         { !user ? (
           <Link href="/login">Login</Link>
