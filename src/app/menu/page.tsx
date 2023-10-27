5cd538b0a0a69258e3f969d2e809873761188d69
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2023-10-27 09:58:44 +0700

    layouting category page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,8 +1,22 @@
+import { menu } from '@/data'
+import Link from 'next/link'
 import React from 'react'
 
 const MenuPage = () => {
   return (
-    <div>MenuPage</div>
+    <div className='p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
+        {
+          menu.map(item => (
+            <Link href={`/menu/${item.slug}`} key={item.id} className='w-full h-1/3 bg-cover p-8 md:h-1/2' style={{backgroundImage: `url(${item.img})`}}>
+              <div className={`text-${item.color} w-1/2`}>
+                <h1 className='uppercase font-bold text-3xl'>{item.title}</h1>
+                <p className='text-sm my-8'>{item.desc}</p>
+                <button className={`hidden 2xl:block bg-${item.color} text-${item.color === "black" ? "white" : "red-500"} py-2 px-4 rounded-md`}>Explore</button>
+              </div>
+            </Link>
+          ))
+        }
+    </div>
   )
 }
 
