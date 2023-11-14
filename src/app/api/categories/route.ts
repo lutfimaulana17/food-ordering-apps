a6156d57e5f93b6f5c59c8410b98ea92e692f239
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2023-11-14 14:29:03 +0700

    setup prisma, docker for postgre db and create api categories

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,24 @@
+import {NextResponse} from "next/server"
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export const GET = async () => {
+    try {
+        const categories = await prisma.category.findMany()
+        return new NextResponse(
+            JSON.stringify(categories),
+            { status: 200 }
+        )
+    } catch (error) {
+        console.log(error)
+        return new NextResponse(
+            JSON.stringify({message: "Something went wrong!"}),
+            { status: 500 }
+        )
+    }
+}
+
+export const POST = () => {
+    return new NextResponse("Heloo", {status: 200})
+}
