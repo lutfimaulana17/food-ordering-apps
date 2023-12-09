2ab3d5e104fd89d559c00f7a782f9df207e7b700
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2023-12-09 10:35:13 +0700

    Integration order page and create feature update status if is admin

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,6 +1,58 @@
+"use client"
+
+import getBaseUrl from '@/helper/getBaseUrl'
+import { OrderType } from '@/types/types'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useSession } from 'next-auth/react'
+import Image from 'next/image'
+import { useRouter } from "next/navigation";
 import React from 'react'
 
 const OrdersPage = () => {
+  const { data:session, status } = useSession()
+
+  const router = useRouter();
+  if (status === "unauthenticated") {
+    router.push("/");
+  }
+
+  const hostApi = getBaseUrl()
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['orders'],
+    queryFn: () =>
+      fetch(`${hostApi}/api/orders`).then(
+        (res) => res.json(),
+      ),
+  })
+
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation({
+    mutationFn:({id, status}: {id: string, status: string})=> {
+      return fetch(`${hostApi}/api/orders/${id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type":"application/json"
+          },
+          body:JSON.stringify(status)
+      })
+    },
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ["orders"] })
+    }
+  })
+
+  const handleUpdate = (e:React.FormEvent<HTMLFormElement>, id:string) => {
+    e.preventDefault()
+    const form = e.target as HTMLFormElement
+    const input = form.elements[0] as HTMLInputElement
+    const status = input.value
+
+    mutation.mutate({id, status})
+  }
+
+  if (isLoading || status === "loading" || status !== "authenticated") return 'Loading...'
+
   return (
     <div className='p-4 lg:px-20 xl:px-40'>
         <table className='w-full border-separate border-spacing-3'>
@@ -14,27 +66,27 @@ const OrdersPage = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr className='text-sm md:text-base bg-red-50'>
-                  <td className='hidden md:block py-6 px-1'>62313721338</td>
-                  <td className='py-6 px-1'>19.07.2023</td>
-                  <td className='py-6 px-1'>70.43</td>
-                  <td className='hidden md:block py-6 px-1'>Big Burger Menu (5), Veggie Pizza (3), Coca Cola 6L (9)</td>
-                  <td className='py-6 px-1'>On the way (approx, 13min)...</td>
-                </tr>
-                <tr className='text-sm md:text-base odd:bg-gray-100'>
-                  <td className='hidden md:block py-6 px-1'>62313721338</td>
-                  <td className='py-6 px-1'>19.07.2023</td>
-                  <td className='py-6 px-1'>70.43</td>
-                  <td className='hidden md:block py-6 px-1'>Big Burger Menu (5), Veggie Pizza (3), Coca Cola 6L (9)</td>
-                  <td className='py-6 px-1'>On the way (approx, 13min)...</td>
-                </tr>
-                <tr className='text-sm md:text-base odd:bg-gray-100'>
-                  <td className='hidden md:block py-6 px-1'>62313721338</td>
-                  <td className='py-6 px-1'>19.07.2023</td>
-                  <td className='py-6 px-1'>70.43</td>
-                  <td className='hidden md:block py-6 px-1'>Big Burger Menu (5), Veggie Pizza (3), Coca Cola 6L (9)</td>
-                  <td className='py-6 px-1'>On the way (approx, 13min)...</td>
-                </tr>
+                {data.map((item: OrderType) => (
+                  <tr className='text-sm md:text-base bg-red-50' key={item.id}>
+                    <td className='hidden md:block py-6 px-1'>{item.id}</td>
+                    <td className='py-6 px-1'>{item.createdAt.toString().slice(0, 10)}</td>
+                    <td className='py-6 px-1'>{item.price}</td>
+                    <td className='hidden md:block py-6 px-1'>{item.products[0].title}</td>
+                    
+                    { session?.user.isAdmin ? (
+                      <td>
+                        <form className='flex items-center justify-center gap-4' onSubmit={(e)=>handleUpdate(e, item.id)}>
+                          <input placeholder={item.status} className='p-2 ring-1 ring-red-100 rounded-md' />
+                          <button className='bg-red-400 p-2 rounded-full'>
+                            <Image src="/edit.png" alt="" width={20} height={20} />
+                          </button>
+                        </form>
+                      </td>
+                    ) : (
+                      <td className='py-6 px-1'>{item.status}</td>
+                    )}
+                  </tr>
+                ))}                
             </tbody>
         </table>
     </div>
