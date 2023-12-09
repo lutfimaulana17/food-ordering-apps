7c1d547a4cedf8663a1afef3151ec7c1b35c0db6
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2023-12-09 22:29:53 +0700

    integrate cart page

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -14,15 +14,15 @@ export const useCartStore = create<CartType & ActionTypes>((set, get)=>({
     addToCart(item) {
         set((state) => ({
             products:[...state.products, item], 
-            totalItems: state.totalItems+item.quantity,
-            totalPrice: state.totalPrice+item.price
+            totalItems: state.totalItems + item.quantity,
+            totalPrice: state.totalPrice + item.price
         }))
     },
     removeFromCart(item) {
         set((state)=>({
             products: state.products.filter(product=> product.id !== item.id),
-            totalItems: state.totalItems-item.quantity,
-            totalPrice: state.totalPrice-item.price
+            totalItems: state.totalItems - item.quantity,
+            totalPrice: state.totalPrice - item.price
         }))
     }
 }))
