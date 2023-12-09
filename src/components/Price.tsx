62d40791b313c06eba8a88d620c65f802272b3a2
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2023-12-09 16:16:15 +0700

    integration detail product page

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,27 +1,25 @@
 "use client"
 
+import { ProductType } from '@/types/types'
 import React, { useEffect, useState } from 'react'
 
-type Props = {
-    price: number;
-    id: number;
-    options?: { title: string; additionalPrice: number; }[]
-}
-
-const Price = ({ price, id, options }: Props) => {
+const Price = ({ product }: {product: ProductType}) => {
+  const { price, options } = product 
   const [total, setTotal] = useState(price)
   const [quantity, setQuantity] = useState(1)
   const [selected, setSelected] = useState(0)  
 
   useEffect(() => {
-    setTotal(quantity * (options ? price + options[selected].additionalPrice : price))
+    if (options?.length) {
+        setTotal(quantity * price + options[selected].additionalPrice)
+    }
   }, [quantity, selected, options, price])
 
   return (
     <div className='flex flex-col gap-4'>
-        <h2 className='text-2xl font-bold'>${total.toFixed(2)}</h2>
+        <h2 className='text-2xl font-bold'>${total}</h2>
         <div className='flex gap-4'>
-            { options?.map((option, index) => (
+            { options?.length && options?.map((option, index) => (
                 <button 
                     key={option.title} 
                     className='p-2 ring-1 ring-red-400 rounded-md min-w-[6rem]'
