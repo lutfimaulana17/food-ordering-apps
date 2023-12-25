0e793dd2aa28cbedfca255c2ec1ee30c753d7067
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2023-12-25 17:25:45 +0700

    create confirm api finish payment

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,8 +1,35 @@
 "use client"
 
+import { useSearchParams, useRouter } from "next/navigation"
+import { useEffect } from "react"
+import getBaseUrl from '@/helper/getBaseUrl'
+
 const SuccessPage = () => {
+  const searchParams = useSearchParams()
+  const payment_intent = searchParams.get("payment_intent")
+  const router = useRouter()
+
+  useEffect(() => {
+    const hostApi = getBaseUrl()
+    const getConfirmPayment = async () => {
+      try {
+        const hostApi = getBaseUrl()
+        await fetch(`${hostApi}/api/confirm/${payment_intent}`, {
+          method: 'PUT'
+        })
+        router.push('/orders')
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getConfirmPayment()
+  }, [payment_intent, router])
+
   return (
-    <div>SuccessPage</div>
+    <div>
+      Payment successful. You are being redirected to the orders page. Please do not close the page.
+    </div>
   )
 }
 
